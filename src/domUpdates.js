8b3863d49f9476f1a3c635c94996834d648777d9
Mandy Alford <mandyalford@gmail.com>
2020-02-29 18:08:11 -0700

    Write fetch requests for agent login and function to greet agent/update headers in summary view section

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -15,23 +15,37 @@ let domUpdates = {
     Promise.all([this.getTravelerData(), this.getTripsData(), this.getDestinationsData()])
       .then(data => {
         this.greetUser(data[0])
-        const traveler = this.instantiateTraveler(data)
+        const travelerData = data[0];
+        const tripsData = data[1];
+        const destinationsData = data[2];
+        const traveler = this.instantiateTraveler(travelerData, tripsData, destinationsData)
         this.displayTrips(traveler.trips);
         this.displayCost(traveler);
       })
       // .catch(error => console.log(error.message));
   },
 
-  instantiateTraveler(data) {
-    const travelerData = data[0];
-    const tripsData = data[1];
-    const destinationsData = data[2];
+  displayAgentInfo() {
+    Promise.all([this.getAllTravelersData(), this.getTripsData(), this.getDestinationsData()])
+      .then(data => {
+        this.greetAgent();
+        const allTravelersData = data[0];
+        const tripsData = data[1];
+        const destinationsData = data[2];
+        const allTravelers = allTravelersData.map((travelerData) => {
+          return this.instantiateTraveler(travelerData, tripsData, destinationsData)
+        })
+        this.displayAllPendingTrips(allTravelers);
+      })
+  },
+
+  instantiateTraveler(travelerData, tripsData, destinationsData) {
     const userTripData = tripsData.filter((tripData) => {
       return tripData.userID == travelerData.id
     })
     const trips = userTripData.map((tripData) => {
-      const currentDestinationData = destinationsData.find((destinationData) => {
-        return destinationData.id === tripData.destinationID
+      const currentDestinationData = destinationsData.find((destinationsData) => {
+        return destinationsData.id === tripData.destinationID
       })
       const destination = new Destination(currentDestinationData)
       tripData.destination = destination
@@ -41,6 +55,13 @@ let domUpdates = {
     travelerData.trips = trips
     let traveler = new Traveler(travelerData)
     return traveler;
+  },
+
+  getAllTravelersData(){
+    return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers')
+      .then(response => response.json())
+      .then(data => data.travelers)
+      .catch(error => console.log(error.message));
   },
 
   getTravelerData() {
@@ -68,12 +89,32 @@ let domUpdates = {
     userGreeting.html(`Welcome, ${data.name}!!!`)
   },
 
+  greetAgent() {
+    userGreeting.html(`Welcome, Agent!!!`);
+    tripsHeader.text(`Trips pending Approval:`);
+    tripsHeader.toggleClass('hidden');
+    spendHeader.toggleClass('hidden');
+  },
+
   displayTrips(trips) {
     let tripsToDisplay = trips.map((trip) => {
       return `<li>${trip.destination.destination} - ${trip.status}</li>`
     }).join('')
     tripsHeader.toggleClass('hidden');
     tripsStatus.html(tripsToDisplay)
+  },
+
+  displayAllPendingTrips(allTravelers) {
+    // get all pending trips
+    //generate html elements
+    //toggle hidden class
+    //insert into html
+    const allPendingTripData = this.getAllPendingTrips(allTravelers)
+
+  },
+
+  getAllPendingTrips(allTravelers) {
+
   },
 
   displayCost(traveler) {
