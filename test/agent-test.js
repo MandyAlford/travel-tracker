72fe3da9606d6503549b8f8669ec98f56cf82526
Mandy Alford <mandyalford@gmail.com>
2020-03-01 16:15:22 -0700

    Write tests for getRevenue method

---
+++ test/agent-test.js
@@ -0,0 +1,76 @@
+import chai from 'chai';
+const expect = chai.expect;
+// import Destination from '../src/destination'
+import Trip from '../src/trip'
+import Traveler from '../src/traveler'
+import Agent from '../src/agent'
+
+
+let trip1;
+let trip2;
+let traveler;
+let agent;
+
+describe('Agent', function() {
+
+  beforeEach(() => {
+    trip1 = new Trip({
+     "id": 1,
+     "userID": 1,
+     "destinationID": 49,
+     "travelers": 2,
+     "date": "2019/09/16",
+     "duration": 4,
+     "status": "pending",
+     "suggestedActivities": [],
+     "destination":
+        {
+        "id": 49,
+        "destination": "Lima, Peru",
+        "estimatedLodgingCostPerDay": 70,
+        "estimatedFlightCostPerPerson": 400,
+        "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+        "alt": "overview of city buildings with a clear sky"
+        }
+     })
+     trip2 = new Trip({
+      "id": 1,
+      "userID": 1,
+      "destinationID": 50,
+      "travelers": 3,
+      "date": "2019/09/16",
+      "duration": 8,
+      "status": "approved",
+      "suggestedActivities": [],
+      "destination":
+         {
+         "id": 50,
+         "destination": "Lima, Peru",
+         "estimatedLodgingCostPerDay": 50,
+         "estimatedFlightCostPerPerson": 300,
+         "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+         "alt": "overview of city buildings with a clear sky"
+         }
+    })
+    traveler = new Traveler({
+     "id": 1,
+     "name": "Ham Leadbeater",
+     "travelerType": "relaxer",
+     "trips": [trip1, trip2]
+     })
+
+    agent = new Agent([traveler]);
+  })
+
+ it('should be a function', function() {
+    expect(Agent).to.be.a('function');
+ });
+
+ it('should have travelers', function() {
+   expect(agent.travelersInfo).to.deep.equal([traveler]);
+ });
+
+ it('should be able to calculate revenue for the last year', function() {
+   expect(agent.getRevenue(traveler.trips)).to.equal(238);
+ })
+})
