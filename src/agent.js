8640e5ef41d4b2970c956f89d0a315cc74ab7e31
Mandy Alford <mandyalford@gmail.com>
2020-03-01 16:14:47 -0700

    Write getRevenue method

--- src/agent.js
+++ src/agent.js
@@ -1,9 +1,21 @@
 import User from './user'
 
 class Agent extends User {
-  constructor() {
+  constructor(allTravelers) {
     super();
+    this.travelersInfo = allTravelers;
+  }
+  getRevenue() {
+    let totalTripsCost = this.travelersInfo.reduce((acc, traveler) => {
+      traveler.trips.forEach((trip) => {
+        acc += trip.calculateTripCost();
+      })
+      return acc;
+    }, 0)
+    return totalTripsCost * .1;
   }
 }
 
 export default Agent;
+
+// 2380
