ae2bea30080c9789f9809c1cb5b41460043bfebd
Mandy Alford <mandyalford@gmail.com>
2020-03-03 21:02:40 -0700

    add user test for verify user

--- test/user-test.js
+++ test/user-test.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 const expect = chai.expect;
 import User from '../src/user'
+import Traveler from '../src/traveler'
 
 
 describe('User', function() {
@@ -9,8 +10,17 @@ describe('User', function() {
   });
 
   it('should be able to verify user', function() {
-    let user = new User()
-    user.verifyUser()
-    expect(User).to.be.a('function');
+    let user = new Traveler({})
+    expect(user.verifyUser('traveler50', 'travel2020')).to.equal(true);
+  });
+
+  it('should be able to deny a user when username is not valid', function() {
+    let user = new Traveler({})
+    expect(user.verifyUser('travelerz', 'travel2020')).to.equal(false);
+  });
+
+  it('should be able to deny a user when password is not valid', function() {
+    let user = new Traveler({})
+    expect(user.verifyUser('traveler50', 'travelz')).to.equal(false);
   });
 });
